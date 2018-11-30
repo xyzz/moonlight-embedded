115daf78341fa2efacce50a249c3ccb5cf495af4
Fábio Neves <eu@fabioneves.com>
2018-11-30 16:49:41 +0000

    Allow alpha-numeric characters so we're able to type a hostname.

--- src/gui/ime.c
+++ src/gui/ime.c
@@ -77,9 +77,9 @@ void initImeDialog(char *title, char *initial_text, int max_text_length) {
   sceImeDialogParamInit(&param);
 
   param.sdkVersion = 0x03150021,
-    param.supportedLanguages = 0x0001FFFF;
+  param.supportedLanguages = 0x0001FFFF;
   param.languagesForced = SCE_TRUE;
-  param.type = SCE_IME_TYPE_EXTENDED_NUMBER;
+  param.type = SCE_IME_TYPE_DEFAULT;
   param.title = ime_title_utf16;
   param.maxTextLength = max_text_length;
   param.initialText = ime_initial_text_utf16;
