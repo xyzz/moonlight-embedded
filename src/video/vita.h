fcf25c5c9ed2fb6bb29f36c19e15e1297f2ad23e
Sunguk Lee <d3m3vilurr@gmail.com>
2020-10-29 20:35:00 +0900

    Detect supported resolutions
    Add some more resolutions and predetect resolutions from it.
    `sceVideodecInitLibrary` would return the negative value if input the
    invalid param (most time it would be invalid resolution)
    
    also it prevent to change the resolution after starting the streaming.
    
    resolve #160

--- src/video/vita.h
+++ src/video/vita.h
@@ -16,9 +16,18 @@
  * You should have received a copy of the GNU General Public License
  * along with Moonlight; if not, see <http://www.gnu.org/licenses/>.
  */
+#include <math.h>
 
+// Vita's sceVideodecInitLibrary only accept resolution that is multiple of 16 on either dimension,
+// and the smallest resolution is 64
+// Full supported resolution list can be found at:
+// https://github.com/MakiseKurisu/vita-sceVideodecInitLibrary-test/
+#define ROUND_NEAREST_16(x)                     (round(((double) (x)) / 16) * 16)
+#define VITA_DECODER_RESOLUTION_LOWER_BOUND(x)  ((x) < 64 ? 64 : (x))
+#define VITA_DECODER_RESOLUTION(x)              (VITA_DECODER_RESOLUTION_LOWER_BOUND(ROUND_NEAREST_16(x)))
 
 void vitavideo_start();
 void vitavideo_stop();
 void vitavideo_show_poor_net_indicator();
 void vitavideo_hide_poor_net_indicator();
+int vitavideo_initialized();
